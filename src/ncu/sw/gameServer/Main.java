e2e6a7b814dd9261d6e6c060a0ac9d7b89a17ac0
Chi-Zer Huang <onlyfly34@Zam-Macbook.local>
2016-11-12 11:49:51 +0800

    Firstly combine server and socket code.

--- src/ncu/sw/gameServer/Main.java
+++ src/ncu/sw/gameServer/Main.java
@@ -1,14 +1,12 @@
 package ncu.sw.gameServer;
 
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 public class Main {
     public static void main(String[] args) {
+        System.out.println("server main");
         //launch(args);
       // ServerGameController server = new ServerGameController(100,20,20);
         Initial a = new Initial(10,100,10);
+        MultiServer server = new MultiServer(9000);
+        new Thread(server).start();
     }
 }
---
+++ src/ncu/sw/gameServer/WorkerThread.java
@@ -0,0 +1,69 @@
+package ncu.sw.gameServer;
+
+/**
+ * Created by NiHao on 2016/10/18.
+ */
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.*;
+import java.net.Socket;
+
+public class WorkerThread implements Runnable{
+    Socket clientSocket = null;
+    String clientStr = "";
+    ObjectOutputStream objOutToClient;
+    int id;
+    public WorkerThread( Socket clientSocket, int id){
+        this.clientSocket = clientSocket;
+        this.id = id;
+        try{
+            objOutToClient = new ObjectOutputStream( clientSocket.getOutputStream() );
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void run(){
+        try{
+
+            /*BufferedReader inFromClient = new
+                    BufferedReader(new InputStreamReader
+                        (clientSocket.getInputStream()));*/
+            DataInputStream inFromClient = new DataInputStream(clientSocket.getInputStream());
+
+            while( true ){
+                clientStr = inFromClient.readUTF();
+                System.out.println( "FromClient "+id+" : " + clientStr );
+                cmd( clientStr );
+                if( clientStr.equals("bye") ){
+                    System.out.println( clientStr );
+                    break;
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void sendToClient( String str){
+        try{
+            DataOutputStream outToClient = new DataOutputStream
+                    (this.clientSocket.getOutputStream());
+            outToClient.writeBytes( str + '\n' );
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void cmd(String str){
+        String[] token = str.split(" ");
+        /*if( token[0].equals("GET") ){
+            //System.out.println("call get func");
+            getTreasure( token[1] );
+        }
+        else if(token[0].equals("RELEASE")){
+            //System.out.println("call get func");
+            releaseTreasure( token[1] );
+        }*/
+    }
+}
